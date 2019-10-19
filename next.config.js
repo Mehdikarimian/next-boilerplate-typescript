69217ea5a5d6019ac219fbbf83526d3c7422cdc4
Mehdi Karimian <mehdi.k@talentbridge.eu.com>
2019-10-19 17:59:04 +0330

    add dotenv

---
+++ next.config.js
@@ -0,0 +1,25 @@
+const webpack = require("webpack");
+// Initialize doteenv library
+require("dotenv").config();
+
+module.exports = {
+  webpack: config => {
+    // Fixes npm packages that depend on `fs` module
+    config.node = {
+      fs: 'empty'
+    }
+    /**
+     * Returns environment variables as an object
+     */
+    const env = Object.keys(process.env).reduce((acc, curr) => {
+      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+      return acc;
+    }, {});
+
+    /** Allows you to create global constants which can be configured
+    * at compile time, which in our case is our environment variables
+    */
+    config.plugins.push(new webpack.DefinePlugin(env));
+    return config
+  }
+}
--- utils/request.ts
+++ utils/request.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com/',
+  baseURL: process.env.BASE_URL,
 });
